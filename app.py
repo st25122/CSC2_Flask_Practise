3d97f5d045c6c25acb43dcdadba7a0dcba023942
st25122 <st25122@ormiston.school.nz>
2026-05-04 12:16:21 +1200

    .

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 
 
 app = Flask(__name__)
-
-
+app.secret_key = '6305'  # Change this to a random secret key
 @app.route("/")
 def index():
     return render_template("index.html")
+if __name__ == '__main__':
+    app.run(debug=True)
